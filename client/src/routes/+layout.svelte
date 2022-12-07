79939757a963bf16f52729be11f91afe8cb58c1b
culas <lucas.schnueriger@me.com>
2022-12-07 16:30:20 +0100

    add some basic styles

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<header>QuizPub, nav will be here</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>Footer</footer>
+
+<style>
+	main {
+		flex-grow: 1;
+		align-self: center;
+		width: 50rem;
+		max-width: 100%;
+		padding: 0 2rem;
+	}
+</style>
