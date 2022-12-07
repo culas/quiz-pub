eac16d7f78d995be2e093f3f7466f6d64851e1b3
culas <lucas.schnueriger@me.com>
2022-12-08 00:25:32 +0100

    visually separate rounds, fix adding questions to keep round order

--- client/src/routes/[code]/edit/+page.svelte
+++ client/src/routes/[code]/edit/+page.svelte
@@ -2,7 +2,11 @@
 	import { page } from '$app/stores';
 	import { saves } from '$lib/stores/saves.store';
 	import type { QuizSave } from 'src/lib/models/quiz-save.model';
-	$: quiz = $saves.find((save: QuizSave) => save.id === $page.params.code) ?? {id: $page.params.code, name: '', rounds: []};
+	$: quiz = $saves.find((save: QuizSave) => save.id === $page.params.code) ?? {
+		id: $page.params.code,
+		name: '',
+		rounds: []
+	};
 
 	function addRound() {
 		quiz.rounds = [...quiz.rounds, { name: '', questions: [''] }];
@@ -11,38 +15,52 @@
 	function addQuestion(rIdx: number) {
 		const round = quiz.rounds[rIdx];
 		round.questions = [...round.questions, ''];
-		quiz.rounds = [...quiz.rounds.filter((_, i) => rIdx !== i), {...round}];
+		quiz.rounds = quiz.rounds.map((r, i) => i === rIdx ? round : r);
 	}
 
 	function save() {
-		$saves = [...$saves.filter(q => q.id !== quiz.id), quiz];
+		$saves = [...$saves.filter((q) => q.id !== quiz.id), quiz];
 	}
 </script>
 
-<h1>Edit Quiz: {quiz.id}</h1>
+<h1>Edit Quiz «{quiz.name}»</h1>
 
-<label>
-	<h2>Name</h2>
-	<input type="text" bind:value={quiz.name} />
-</label>
+<section>
+	<label>
+		<h2>Name</h2>
+		<input type="text" bind:value={quiz.name} />
+	</label>
+</section>
 
 {#each quiz.rounds as round, rIdx}
-	<article class="round">
+	<section class="round">
 		<label>
-			<h3>Round</h3>
+			<h2>Round</h2>
 			<input type="text" bind:value={round.name} />
 		</label>
 
 		{#each round.questions as question, qIdx}
 			<label>
-				<h4>Question {qIdx}</h4>
+				<h3>Q{qIdx + 1}</h3>
 				<input type="text" bind:value={question} />
 			</label>
 		{/each}
 		<button on:click={() => addQuestion(rIdx)}>Add question</button>
-	</article>
+	</section>
 {/each}
 
-<button on:click={addRound}>Add round</button>
+<section>
+	<button on:click={addRound}>Add round</button>
+</section>
 
 <button on:click={save}>Save</button>
+
+<style>
+	section {
+		background-color: var(--color-white);
+		padding: 1rem;
+		margin: 1rem 0;
+		border-radius: 0.5rem;
+		box-shadow: var(--box-shadow);
+	}
+</style>
