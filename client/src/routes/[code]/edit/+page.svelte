947285189cba56449b4fa225a203df2814a98656
culas <lucas.schnueriger@me.com>
2022-12-08 01:29:17 +0100

    add buttons to remove questions and rounds

--- client/src/routes/[code]/edit/+page.svelte
+++ client/src/routes/[code]/edit/+page.svelte
@@ -20,6 +20,16 @@
 		quiz.rounds = quiz.rounds.map((r, i) => (i === rIdx ? round : r));
 	}
 
+	function removeQuestion(rIdx: number, qIdx: number): any {
+		quiz.rounds = quiz.rounds.map((r, i) =>
+			i === rIdx ? { ...r, questions: r.questions.filter((_, i) => i !== qIdx) } : r
+		);
+	}
+
+	function removeRound(rIdx: number): any {
+		quiz.rounds = quiz.rounds.filter((_, i) => i !== rIdx);
+	}
+
 	function save() {
 		quiz.date = Date.now();
 		$saves = [...$saves.filter((q) => q.id !== quiz.id), quiz];
@@ -40,20 +50,42 @@
 		<label>
 			<h2>Round</h2>
 			<input type="text" bind:value={round.name} />
+			<button on:click={() => removeRound(rIdx)}>delete round</button>
 		</label>
 
 		{#each round.questions as question, qIdx}
 			<label>
 				<h3>Q{qIdx + 1}</h3>
 				<input type="text" bind:value={question} />
+				<button on:click={() => removeQuestion(rIdx, qIdx)}>delete question</button>
 			</label>
 		{/each}
-		<button on:click={() => addQuestion(rIdx)}>Add question</button>
+		<button on:click={() => addQuestion(rIdx)}>add question</button>
 	</section>
 {/each}
 
 <section>
-	<button on:click={addRound}>Add round</button>
+	<button on:click={addRound}>add round</button>
 </section>
 
-<button on:click={save}>Save</button>
+<button on:click={save}>save</button>
+
+<style>
+	label {
+		display: flex;
+		flex-wrap: wrap;
+		align-items: flex-end;
+		margin-bottom: 1rem;
+		gap: 0.5rem;
+	}
+
+	label h2,
+	label h3 {
+		width: 100%;
+	}
+
+	label input {
+		width: auto;
+		flex-grow: 1;
+	}
+</style>
