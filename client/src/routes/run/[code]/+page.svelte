58494ed2ab4b734085809d079dd6ad7a08725a15
culas <lucas.schnueriger@me.com>
2022-12-10 13:48:20 +0100

    run quiz page prototype

--- client/src/routes/run/[code]/+page.svelte
+++ client/src/routes/run/[code]/+page.svelte
@@ -1,7 +1,139 @@
-<script>
+<script lang="ts">
 	import { page } from '$app/stores';
-	import { websocket } from '$lib/utils/websocket';
-	$websocket;
+	import type { QuizInfo, SocketMessage } from '$lib/models/messages';
+	import type { QuizRun } from '$lib/models/quiz-run.model';
+	import { connectSocket } from '$lib/utils/websocket';
+	import { writable } from 'svelte-local-storage-store';
+	const run = writable($page.params.code, {} as QuizRun);
+
+	const socket = connectSocket(
+		new Map([
+			['joinCode', $run.joinCode],
+			['adminCode', $run.adminCode]
+		])
+	);
+	$socket = {
+		type: 'quiz-info',
+		name: $run.name,
+		rounds: $run.rounds.length,
+		questions: $run.rounds.reduce((sum, r) => sum + r.questions.length, 0)
+	} as QuizInfo;
+
+	$: updateRun($socket);
+
+	function updateRun(msg: SocketMessage) {
+		if (msg.type === 'players') {
+			$run = { ...$run, players: msg.players };
+		}
+		if (msg.type === 'answers') {
+			$run = {
+				...$run,
+				rounds: $run.rounds.map((r, i) =>
+					i === $run.currentRound
+						? {
+								...r,
+								questions: r.questions.map((q, qi) => ({
+									...q,
+									answers: [...q.answers, { player: msg.player, answer: msg.answers[qi] }]
+								}))
+						  }
+						: r
+				)
+			};
+			if (currentRound.questions.every((q) => q.answers.length === $run.players.length)) {
+				$run = { ...$run, state: 'revealing' };
+			}
+		}
+	}
+
+	function nextRound() {
+		if ($run.currentRound < $run.rounds.length - 1) {
+			$run = {
+				...$run,
+				state: 'answering',
+				currentRound: $run.currentRound + 1
+			};
+		} else {
+			$run = { ...$run, state: 'finished' };
+		}
+	}
+
+	function score(question: number, player: string, points: number) {
+		$run = {
+			...$run,
+			rounds: $run.rounds.map((r, i) =>
+				i === $run.currentRound
+					? {
+							...r,
+							questions: r.questions.map((q, qi) =>
+								qi === question
+									? {
+											...q,
+											answers: q.answers.map((a) =>
+												a.player === player ? { ...a, score: points } : a
+											)
+									  }
+									: q
+							)
+					  }
+					: r
+			)
+		};
+		if (currentRound.questions.every((q) => q.answers.every((a) => a.score))) {
+			nextRound();
+		}
+	}
+
+	function reveal(qIdx: number, player: string) {
+		console.warn('not yet implemented');
+	}
+
+	$: currentRound = $run.rounds[$run.currentRound];
 </script>
 
-<h1>Run Quiz «{$page.params.code}»</h1>
+<h1>Run Quiz «{$run.name}»</h1>
+
+{#if $run.state === 'preparation'}
+	<p>Waiting for players to join with code: <b>{$run.joinCode}</b></p>
+	<ul>
+		{#each $run.players as p}
+			<li style="color: {p.color}">{p.name}</li>
+		{/each}
+	</ul>
+	<button on:click={() => nextRound()}>Start first round</button>
+{:else if $run.state === 'answering'}
+	<h2>Round: {currentRound.name}</h2>
+	{#each currentRound?.questions as q, qi}
+		<p>{q.text}</p>
+		<ul>
+			{#each q.answers as a}
+				<li>{a.player}</li>
+			{/each}
+		</ul>
+	{/each}
+{:else if $run.state === 'revealing'}
+	<p>Reveal answers</p>
+	<h2>Round: {currentRound.name}</h2>
+	{#each currentRound?.questions as q, qi}
+		<p>{q.text}</p>
+		<ul>
+			{#each q.answers as a}
+				<li>
+					{a.player}: {a.answer} <button on:click={() => reveal(qi, a.player)}>Reveal</button>
+				</li>
+			{/each}
+		</ul>
+	{/each}
+{:else if $run.state === 'scoring'}
+	<h2>Round: {currentRound.name}</h2>
+	{#each currentRound?.questions as q, qi}
+		<p>{q.text}</p>
+		<ul>
+			{#each q.answers as a}
+				<li>{a.player}: {a.answer} <button on:click={() => score(qi, a.player, 1)}>OK</button></li>
+			{/each}
+		</ul>
+	{/each}
+{:else if $run.state === 'finished'}
+	<p>Finish the quiz</p>
+{/if}
