e30cb49faee201274655524125df365e0962a6f4
culas <lucas.schnueriger@me.com>
2022-12-11 16:53:33 +0100

    improve state checks

--- client/src/routes/xstate/+page.svelte
+++ client/src/routes/xstate/+page.svelte
@@ -4,7 +4,7 @@
 	import { quizMachine } from '$lib/stores/xstate';
 	import { writable } from 'svelte-local-storage-store';
 	import { interpret } from 'xstate';
-	$: console.log($qService.value);
+	$: console.log($qService.value, $qService.matches('round'));
 	$: console.table($qService.context.answers);
 	const run = writable('RYKNG1', {} as QuizRun);
 	const qService = interpret(
@@ -38,10 +38,10 @@
 </script>
 
 <div>
-	{#if $qService.value === 'lobby'}
+	{#if $qService.matches('lobby')}
 		<PlayerList players={$qService.context.players} />
 		<button on:click={() => qService.send('START')}>start</button>
-	{:else if $qService.value.round}
+	{:else if $qService.matches('round')}
 		<h2>{$qService.context.rounds[cr].text}</h2>
 		{#each $qService.context.questions.filter((q) => q.roundId === cr) as q}
 			<p><b>Q{q.id + 1}: {q.text}</b></p>
@@ -50,11 +50,11 @@
 					<div>
 						<span>{a.player}</span>
 						<span>{a.score !== undefined ? '(' + a.score + ')' : ''}</span>
-						{#if $qService.value.round === 'revealing'}
+						{#if $qService.matches('round.revealing')}
 							<button on:click={() => qService.send('REVEAL', { qIdx: q.id, player: a.player })}
 								>reveal</button
 							>
-						{:else if $qService.value.round === 'scoring'}
+						{:else if $qService.matches('round.scoring')}
 							<button
 								on:click={() => qService.send('SCORE', { qIdx: q.id, player: a.player, score: 1 })}
 								>1</button
@@ -68,15 +68,15 @@
 					<p>{a.revealed ? a.text : '***'}</p>
 				</section>
 			{/each}
-			{#if $qService.value.round === 'answering'}
+			{#if $qService.matches('round.answering')}
 				<input type="text" bind:value={answers[q.id]} />
 			{/if}
 		{/each}
-		{#if $qService.value.round === 'answering'}
+		{#if $qService.matches('round.answering')}
 			<button on:click={() => sendAnswers('celep')}>answer celep</button>
 			<button on:click={() => sendAnswers('matte')}>answer matte</button>
 		{/if}
-	{:else if $qService.value === 'result'}
+	{:else if $qService.matches('result')}
 		<h2>Scores</h2>
 		{#each $qService.context.players as player}
 			<p>
