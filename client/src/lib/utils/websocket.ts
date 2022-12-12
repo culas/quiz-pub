9c4029f5e937ca02e347d2fc8a37831b317e91f2
culas <lucas.schnueriger@me.com>
2022-12-12 22:04:00 +0100

    remove not needed code for handling websockets

--- client/src/lib/utils/websocket.ts
+++ client/src/lib/utils/websocket.ts
@@ -2,8 +2,6 @@ import websocketStore from '$lib/stores/websocket.store';
 import type { StateEvent } from '$server-interface/events.model';
 import type { SocketMessage } from '$server-interface/messages';
 
-let socket: WebSocket;
-
 export function connectSocket(params?: Map<string, string>) {
 	let { host, protocol } = window.location;
 	if (host.includes('localhost')) host = 'localhost:42069';
@@ -17,44 +15,3 @@ export function connectSocket(params?: Map<string, string>) {
 }
 
 export const websocket = connectSocket();
-
-const startSocketListeners = (socket: WebSocket) => {
-	socket.onmessage = (event) => {
-		console.log("Message from server:");
-		console.log(event);
-		console.log(JSON.parse(event.data));
-
-		handle(JSON.parse(event.data));
-	};
-
-	socket.onclose = (event) => {
-		console.log("Connection closed:");
-		console.log(event);
-	};
-
-	socket.onerror = (event) => {
-		console.log("Connection error:");
-		console.log(event);
-	};
-};
-
-interface ClientWebsocketMessages { }
-export const sendMessage = (msg: ClientWebsocketMessages) => {
-	socket?.send(JSON.stringify(msg));
-};
-
-interface ServerWebsocketMessages { }
-export function handle(message: ServerWebsocketMessages) {
-	// switch (message.type) {
-	//   case "gamesession":
-	// 	loadSession(message.sessionId, message.playerColor);
-	// 	break;
-	//   case "gamecontext":
-	// 	updateGame(message);
-	// 	navigateToGameIfNecessary();
-	// 	break;
-	//   case "menu":
-	// 	navigateToMenu();
-	// 	break;
-	// }
-}
