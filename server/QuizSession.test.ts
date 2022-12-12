dd974d878bf94b3920b7e97c654fe7d977f14d7d
culas <lucas.schnueriger@me.com>
2022-12-12 21:40:52 +0100

    add placeholder test

---
+++ server/QuizSession.test.ts
@@ -0,0 +1,7 @@
+import { assertExists } from "asserts";
+import { QuizSession } from "./QuizSession.ts";
+
+Deno.test("generic test placeholder", () => {
+	const quizSession = new QuizSession('ADMIN', 'JOIN', {} as WebSocket);
+	assertExists(quizSession)
+});
