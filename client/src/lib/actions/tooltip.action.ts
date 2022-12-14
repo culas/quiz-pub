60bf56d913cc30ad4157169d9080a36dbdaacd04
culas <lucas.schnueriger@me.com>
2022-12-14 16:51:14 +0100

    add tooltip component and action from Svelte REPL

---
+++ client/src/lib/actions/tooltip.action.ts
@@ -0,0 +1,49 @@
+import Tooltip from '$lib/components/Tooltip.svelte';
+
+export const tooltip = (element: HTMLElement) => {
+
+  let tooltipText: string | null;
+  let tooltipComponent: Tooltip;
+
+  const addTooltip = () => {
+    tooltipText = element.getAttribute('data-tooltip');
+
+    tooltipComponent = new Tooltip({
+      target: document.body,
+      props: {
+        tooltipText,
+        xAxis: 0,
+        yAxis: 0
+      }
+    });
+
+    const tooltipDiv = document.querySelectorAll<HTMLElement>('.tooltip');
+    const parentCoords = element.getBoundingClientRect();
+
+    tooltipDiv.forEach((i) => {
+      const top = parentCoords.top + window.scrollY - i.offsetHeight - 10;
+      const center = parentCoords.left + window.scrollX + (element.offsetWidth - i.offsetWidth) / 2;
+
+      tooltipComponent.$set({
+        xAxis: center,
+        yAxis: top
+      });
+    });
+
+  }
+
+  const removeTooltip = () => {
+    tooltipComponent.$destroy();
+  }
+
+  element.addEventListener('mouseover', addTooltip);
+  element.addEventListener('mouseout', removeTooltip);
+
+  return {
+    destroy() {
+      removeTooltip();
+      element.removeEventListener('mouseover', addTooltip);
+      element.removeEventListener('mouseout', removeTooltip);
+    }
+  }
+}
---
+++ client/src/lib/components/Tooltip.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let tooltipText: string | null;
+	export let xAxis: number;
+	export let yAxis: number;
+</script>
+
+<div class="tooltip" role="tooltip" style="left: {xAxis}px; top: {yAxis}px;">
+	{tooltipText}
+</div>
+
+<style>
+	.tooltip {
+		color: #fff;
+		min-width: 80px;
+		max-width: 250px;
+		position: absolute;
+		padding: 0.5rem 0.75rem;
+		text-align: center;
+		font-weight: 400;
+		font-size: 0.7rem;
+		line-height: 1.2;
+		border-radius: 0.25rem;
+		background-color: rgba(0, 0, 0, 0.8);
+	}
+</style>
