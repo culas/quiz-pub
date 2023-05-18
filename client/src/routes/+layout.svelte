ee667ab3ecbcfa64020f88f05dfdd81f10b78d46
culas <lucas.schnueriger@me.com>
2023-05-18 16:50:23 +0200

    add app shell and use skeleton light switch

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -4,55 +4,26 @@
 	import '../app.postcss';
 	import Navigation from '$lib/components/Navigation.svelte';
 	import { fullTitle } from '$lib/stores/title.store';
+	import { AppShell, LightSwitch, setInitialClassState } from '@skeletonlabs/skeleton';
 
 	$: document.title = $fullTitle;
+	setInitialClassState();
 </script>
 
-<header>
-	<a href="/">QuizPub</a>
-	<Navigation />
-	<DarkmodeToggle />
-</header>
+<AppShell>
+	<svelte:fragment slot="header">
+		<a href="/">QuizPub</a>
+		<Navigation />
+		<LightSwitch></LightSwitch>
+	</svelte:fragment>
 
-<main>
-	<slot />
-</main>
+	<main>
+		<slot />
+	</main>
 
-<footer>
-	QuizPub: create, run and play pub quiz style events • by celep for Fishcord • <a
+	<svelte:fragment slot="footer">
+		QuizPub: create, run and play pub quiz style events &#x2022; by celep for Fishcord &#x2022; <a
 		href="https://github.com/culas/quiz-pub">GitHub</a
 	>
-</footer>
-
-<style>
-	main {
-		flex-grow: 1;
-		align-self: center;
-		width: 50rem;
-		max-width: 100%;
-		padding: 0 2rem;
-	}
-
-	header,
-	footer {
-		padding: 0.5rem;
-		background-color: var(--color-light);
-	}
-
-	header {
-		display: flex;
-		gap: 1rem;
-		align-items: center;
-	}
-
-	footer {
-		font-size: 0.75rem;
-	}
-
-	header a {
-		color: var(--color-text);
-		text-decoration: none;
-		font-size: 1.2rem;
-		font-weight: bold;
-	}
-</style>
+	</svelte:fragment>
+</AppShell>
